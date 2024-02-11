c421f176cd49bb9f5443e842adfeaa6a1befe0f6
Roel van der Steen <roel@tacx.com>
2024-02-11 22:53:45 +0100

    Add new command line application ReSignMsixBundle
    This commit adds a new command-line application called ReSignMsixBundle. This application can be used to change the code signing certificate for an MSIX bundle installer, using Azure Key Vault.

---
+++ src/ReSignMsixBundle/BusinessLogic/PackagePublisherTool.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace ReSignMsixBundle.BusinessLogic;
+
+[SupportedOSPlatform("windows")] internal static class PackagePublisherTool
+{
+    /// <summary>Modify the package publisher in the manifests of a list of MSIX files.</summary>
+    /// <param name="msixFiles">The MSIX files.</param>
+    /// <param name="publisher">The publisher.</param>
+    /// <param name="cancellationToken">A token that allows processing to be cancelled.</param>
+    public static void ModifyPackagePublisher(IEnumerable<string> msixFiles, string publisher, CancellationToken cancellationToken)
+    {
+        foreach (var msixFile in msixFiles)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            ModifyPackagePublisher(msixFile, publisher);
+        }
+    }
+
+    private static void ModifyPackagePublisher(string msixFile, string publisher)
+    {
+        var manifestPath = string.Empty;
+        using (var zip = ZipFile.OpenRead(msixFile))
+        {
+            foreach (var entry in zip.Entries)
+            {
+                if (!entry.Name.Equals("AppxManifest.xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                manifestPath = Path.Combine(Path.GetTempPath(), entry.Name);
+                entry.ExtractToFile(manifestPath, true);
+
+                ModifyPublisherInManifest(manifestPath, publisher);
+                break;
+            }
+        }
+
+        if (string.IsNullOrEmpty(manifestPath))
+        {
+            throw new InvalidOperationException($"AppxManifest.xml was not found in {msixFile}");
+        }
+
+        new AppxPackageEditorWrapper().UpdatePackageManifest(msixFile, manifestPath);
+    }
+
+    private static void ModifyPublisherInManifest(string manifestPath, string publisher)
+    {
+        var xmlDoc = XDocument.Load(manifestPath);
+        if (xmlDoc.XPathEvaluate("//*[local-name()='Identity']/@Publisher") is XAttribute publisherAttribute)
+        {
+            publisherAttribute.Value = publisher;
+        }
+
+        xmlDoc.Save(manifestPath);
+    }
+}
